a8434ad2f8d56ff2a8f5161a5e42b766d3e67cc8
unknown <anduhit@gmail.com>
2023-03-22 21:21:40 +0300

    added infrastructure project

--- HR.LeaveManagement.Clean/HR.LeaveManagement.Domain/Common/BaseEntity.cs
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Domain/Common/BaseEntity.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public DateTime? DateCreated { get; set; }
-        public DateTime? LastModifiedDate { get; set; }
+        public DateTime? DateModified { get; set; }
     }
 }
---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Persistence/PersistanceServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Persistence;
+
+public static class PersistanceServiceRegistration
+{
+    public static IServiceCollection AddPersistanceServices(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
